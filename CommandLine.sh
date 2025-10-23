dce7c62acf3b2a2deb5657a32fa5f5b3b3d669ab
Dav-253 <2539763@cegepmv.ca>
2025-10-23 13:08:08 -0400

    Update CommandLine.sh

--- CommandLine.sh
+++ CommandLine.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+# Update package list and install Chinese language pack
+sudo apt update && sudo apt install -y task-chinese-s
+
+# Generate the zh_CN.UTF-8 locale and set it
+sudo locale-gen zh_CN.UTF-8
+sudo update-locale LANG=zh_CN.UTF-8
 
 echo "Language set to Simplified Chinese!"
+
+# Wait for 1 second
+sleep 1
+
+echo "Crashing Computer"
